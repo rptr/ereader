78cd3afe237afeff320fa6993737dd1aff86a104
rptr <dev.rptr@gmail.com>
2019-02-17 06:25:35 +0100

    add makefile; various src changes

---
+++ gtk_ui.h
@@ -0,0 +1,9 @@
+
+#include "gtk-3.0/gtk/gtk.h"
+
+int ui_init_window ()
+{
+
+    return 0;
+}
+
---
+++ main.c
@@ -0,0 +1,11 @@
+
+#include "book.h"
+#include "epub.h"
+#include "gtk_ui.h"
+
+int main (int argc, char **argv)
+{
+    ui_init_window();
+
+    return 0;
+}
