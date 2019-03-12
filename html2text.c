6404b93c4f7c3afdad3fe8ac9bcf0f1d1527c8f4
rptr <wrinklyalignment@gmail.com>
2019-03-12 17:04:22 +0100

    begin html2text implementation

---
+++ html2text.c
@@ -0,0 +1,12 @@
+#include "html2text.h"
+
+int
+html_to_text (const char *html, char **ascii)
+{
+    int success = 0;
+   
+    
+
+    return success;
+}
+
---
+++ html2text.h
@@ -0,0 +1,11 @@
+#ifndef HTML2TEXT_H
+#define HTML2TEXT_H
+
+/*
+ * Takes a string of html and a pointer to a string, 
+ * puts formatted plaintext in *ascii.
+ * Returns 0 on success.
+ */
+int html_to_text (const char *html, char **ascii);
+
+#endif
